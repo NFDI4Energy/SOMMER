270bb328770f225ac327c9dfdb6b06c58c0070c8
Hoyer-Klick <Carsten.Hoyer-Klick@dlr.de>
2025-05-11 18:33:54 +0200

    Update to register_new_group

--- Snippets/register_new_group.py
+++ Snippets/register_new_group.py
@@ -1,9 +1,8 @@
 import json
-import os
 import requests
 
-API_KEY = "b3b0ce9e-5873-49f7-9bd8-be3e4ae95916"
-ACCOUNT_NAME = "choyerklick"
+API_KEY = "XXXXX"
+ACCOUNT_NAME = "YYYYY"
 # OR jsut hard-code it if you do not want to change your env variables
 
 DATABUS_URI_BASE = "https://databus.dev.dbpedia.link"
